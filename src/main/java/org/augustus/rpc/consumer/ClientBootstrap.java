557c9068c5681729f98f39dbcd4b7aaff2a1e3c1
lyjady <lyj2780866735>
2020-04-29 20:57:04 +0800

    RPC实现

--- src/main/java/org/augustus/rpc/consumer/ClientBootstrap.java
+++ src/main/java/org/augustus/rpc/consumer/ClientBootstrap.java
@@ -1,9 +1,17 @@
 package org.augustus.rpc.consumer;
 
+import org.augustus.rpc.api.HelloService;
+
 /**
  * @author LinYongJin
  * @date 2020/4/28 21:43
  */
 public class ClientBootstrap {
 
+    public static void main(String[] args) {
+        NettyClient customer = new NettyClient();
+        HelloService service = (HelloService) customer.getBean(HelloService.class, "HelloService#hello#");
+        String res = service.hello("RPC~~~");
+        System.out.println("调用的结果 res= " + res);
+    }
 }
--- src/main/java/org/augustus/rpc/consumer/NettyClient.java
+++ src/main/java/org/augustus/rpc/consumer/NettyClient.java
@@ -1,10 +1,61 @@
 package org.augustus.rpc.consumer;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import org.augustus.netty.simple.NettyClientHandler;
+
+import java.lang.reflect.Proxy;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author LinYongJin
  * @date 2020/4/28 21:43
  */
 public class NettyClient {
 
+    private static ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
+    private static ClientHandler handler;
+
+    public Object getBean(final Class<?> serviceClass, final String providerName) {
+        return Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class<?>[]{serviceClass}, (name, method, args) -> {
+            if (handler == null) {
+                init();
+            }
+            handler.setParam(providerName + args[0]);
+            return executorService.submit(handler).get();
+        });
+    }
+
+    private static void init() {
+        handler = new ClientHandler();
+        EventLoopGroup loopGroup = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(loopGroup)
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.TCP_NODELAY, true)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast(new StringDecoder());
+                        pipeline.addLast(new StringEncoder());
+                        pipeline.addLast(handler);
+                    }
+                });
+        try {
+            bootstrap.connect("127.0.0.1", 8080).sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
